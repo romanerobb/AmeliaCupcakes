2237149c1f927c22ff2e2cd8dcf21d69ca1128ff
Romane Orlando Robb <mail@romanerobb.com>
2023-01-03 17:21:17 -0500

    Updated files.

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Romane Robb
+# Fun Fact: I love finance.
+
+print("  RRR   R     ")
+print(" R   R  R     ")
+print(" R      R     ")
+print("  RRR   R     ")
+print("     R  R     ")
+print(" R   R  R     ")
+print("  RRR   RRRRR ")
